d7c314d01d356a38073a29590684bc499e93c03a
nicolas-trindade <nicolastrindade@hotmail.com>
2023-02-11 03:14:33 -0300

    first commit; project create

---
+++ estudo/src/main/java/com/politica/projeto/repositorio/estudo/RepositorioEstudoApiPoliticaApplication.java
@@ -0,0 +1,13 @@
+package com.politica.projeto.repositorio.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioEstudoApiPoliticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioEstudoApiPoliticaApplication.class, args);
+	}
+
+}
---
+++ estudo/src/test/java/com/politica/projeto/repositorio/estudo/RepositorioEstudoApiPoliticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politica.projeto.repositorio.estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioEstudoApiPoliticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
