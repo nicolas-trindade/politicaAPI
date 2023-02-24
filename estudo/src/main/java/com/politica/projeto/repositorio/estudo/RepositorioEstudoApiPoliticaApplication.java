b3ed086b7b55253bdd9a497b7d67740007c06414
nicolas-trindade <nicolastrindade@hotmail.com>
2023-02-23 23:31:23 -0300

    add import LOGGER in project

--- estudo/src/main/java/com/politica/projeto/repositorio/estudo/RepositorioEstudoApiPoliticaApplication.java
+++ estudo/src/main/java/com/politica/projeto/repositorio/estudo/RepositorioEstudoApiPoliticaApplication.java
@@ -1,10 +1,14 @@
 package com.politica.projeto.repositorio.estudo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RepositorioEstudoApiPoliticaApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RepositorioEstudoApiPoliticaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RepositorioEstudoApiPoliticaApplication.class, args);
